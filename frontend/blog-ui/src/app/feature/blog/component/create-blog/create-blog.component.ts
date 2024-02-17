1e8862b7c0213407b49ecd580aa2463c88569899
P.Raut <p.raut@serrala.com>
2024-02-17 13:53:00 +0530

    create blog page design updated

--- frontend/blog-ui/src/app/feature/blog/component/create-blog/create-blog.component.ts
+++ frontend/blog-ui/src/app/feature/blog/component/create-blog/create-blog.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-blog',
   templateUrl: './create-blog.component.html',
   styleUrls: ['./create-blog.component.scss']
 })
-export class CreateBlogComponent {
+export class CreateBlogComponent implements OnInit {
+  formData: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.formData = this.fb.group({
+      "blogName": new FormControl("", Validators.required),
+      "blogSummary": new FormControl("", Validators.required),
+      "blogAuthor": new FormControl("", Validators.required),
+      "blogData": new FormControl("", Validators.required)
+    })
+  }
+  ngOnInit(): void {
+
+  }
+
+  saveBlog(){
+    console.log(this.formData.value);
+  }
 }
